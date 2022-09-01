98596f0912160159b96f867bb9ad2b19066a11c3
shargrove09 <scott7c1@gmail.com>
2022-09-01 17:40:20 -0400

    Basic Nav Drawer to Main Nav

--- src/components/NavDrawer.js
+++ src/components/NavDrawer.js
@@ -10,13 +10,19 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
+import { IconButton } from "@material-ui/core";
+import MenuIcon from "@mui/icons-material/Menu";
+
+
 
 export default function TemporaryDrawer() {
   const [state, setState] = React.useState({
     left: false,
   });
 
-  const toggleDrawer = (anchor, open) => (event) => {
+  const [isOpen,setOpen] = React.useState(false); 
+
+  const toggleDrawer = (open) => (event) => {
     if (
       event.type === "keydown" &&
       (event.key === "Tab" || event.key === "Shift")
@@ -24,15 +30,15 @@ export default function TemporaryDrawer() {
       return;
     }
 
-    setState({ ...state, [anchor]: open });
+    setOpen(open);
   };
 
-  const list = (anchor) => (
+  const list = () => (
     <Box
-      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      //sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
       role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
     >
       <List>
         {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
@@ -63,19 +69,26 @@ export default function TemporaryDrawer() {
   );
 
   return (
-    <div>
-      {["left"].map((anchor) => (
-        <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+    <div className="TEST">
+      <IconButton
+                  size="small"
+                  edge="start"
+                  color="inherit"
+                  aria-label="menu"
+                  onClick={toggleDrawer(true)}
+                  //sx={{ mr: 2 }}
+                >
+                  <MenuIcon />
+          
           <Drawer
-            anchor={anchor}
-            open={state[anchor]}
-            onClose={toggleDrawer(anchor, false)}
+            anchor="left"
+            open={isOpen}
+            onClose={toggleDrawer(false)}
+            onClick
           >
-            {list(anchor)}
+            {list()}
           </Drawer>
-        </React.Fragment>
-      ))}
+          </IconButton>
     </div>
   );
 }
