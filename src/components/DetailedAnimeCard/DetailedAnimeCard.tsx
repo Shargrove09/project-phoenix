6a043a5af99ea5ecb7f930c4d64b41b1ce900182
Shargrove <scott.hargrove@reliabotics.com>
2023-10-30 15:04:53 -0400

    Home and Results rework

--- src/components/DetailedAnimeCard/DetailedAnimeCard.tsx
+++ src/components/DetailedAnimeCard/DetailedAnimeCard.tsx
@@ -1,8 +1,10 @@
 import { Card, CardActions, CardContent, CardMedia } from "@mui/material";
 import React from "react";
+import Typography from "@mui/material/Typography";
+
+import { Anime } from "../../common/Anime";
 
 import "./DetailedAnimeCard.scss";
-import { Anime } from "../../common/Anime";
 
 interface Props {
   animeData: Anime;
@@ -11,14 +13,22 @@ interface Props {
 const DetailedAnimeCard = (props: Props) => {
   const { animeData } = props;
   return (
-    <Card className="dcard__container">
-      {" "}
-      {animeData.title}
-      <CardMedia
-        sx={{ height: 140, width: 140 }}
-        image={animeData.images.jpg.image_url}
-        title={`${animeData.title}_cover_picture`}
-      />
+    <Card sx={{ backgroundColor: "#424242" }} className="dcard__container">
+      <div className="dcard__header">
+        <Typography className="dcard__header_text" variant="h5">
+          {animeData.title}
+        </Typography>
+      </div>
+
+      <CardContent className="dcard__content">
+        <CardMedia
+          sx={{ height: 120, width: 84 }}
+          image={animeData.images.jpg.image_url}
+          title={`${animeData.title}_cover_picture`}
+          className="dcard__card_media"
+        />
+        <Typography> {animeData.synopsis}</Typography>
+      </CardContent>
     </Card>
   );
 };
